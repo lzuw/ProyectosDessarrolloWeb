0c34d381bd19960ed4c4086f4747a6437538c333
Dylan <crcdylan@gmail.com>
2026-06-23 18:13:03 -0600

    Clase 5

--- Clase_4_Base/src/main/java/com/ufide/clase4base/Clase4baseApplication.java
+++ Clase_4_Base/src/main/java/com/ufide/clase4base/Clase4baseApplication.java
@@ -3,11 +3,7 @@ package com.ufide.clase4base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Punto de entrada de la aplicacion.
- * Spring escanea automaticamente este paquete y sus sub-paquetes
- * buscando @Controller, @Service, @Repository, @Entity, etc.
- */
+
 @SpringBootApplication
 public class Clase4baseApplication {
 
